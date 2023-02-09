ec45b6c670473d5af984ca761913fb98ce54acee
jeevithasandeep <78718389+jeevithasandeep@users.noreply.github.com>
2023-02-09 19:31:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function preload() {
+}
+
+function setup() {
+  canvas = createCanvas(640, 480);
+  canvas.position(110, 250);
+ 
+
+ 
+}
+
+function draw() {
+    fill("orange");
+    stroke("black");
+ circle(60,50,40);
+ fill("red");
+    stroke("black");
+ rect(100,120,70,50);
+ fill("purple");
+    stroke("black");
+ ellipse(500, 320, 355, 155);
+}
+
+
